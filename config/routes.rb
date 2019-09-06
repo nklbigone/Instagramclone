e4ed399e3ba9d3eb5bc13c3cf0b69cae2edf4bb9
PeterYves <iradupeteryves@gmail.com>
2019-09-06 21:33:36 +0200

    instagram clone with added favorites page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   end
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   root to: 'posts#index'
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy, :index]
 end
